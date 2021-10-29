cc5aeeb67e83403674f36bc92d8148cdb24a6695
Andrea Biondo <andreabiondo2000@gmail.com>
2021-10-29 11:31:22 +0200

    aggiornamento navbar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-       
+      
       </header>
+      <div className="navBar">
+      <div className="button3">
+      Visualizza repos – Aggiorna repos – Nuovo utente
+      </div>
+      <div className="logo"> LOGO </div>
+      </div>
+      <div className="tablebox">box 2</div>
     </div>
   );
 }
